25cf5d9f690dc2a750cc4aa344eba0e20cfe2f8f
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-09-14 22:35:54 -0300

    fix: make card drag work and style home
    Co-authored-by: Luís Furtado <luiscesm1@gmail.com>

--- src/components/PossibleClientsList/PossibleClientsList.js
+++ src/components/PossibleClientsList/PossibleClientsList.js
@@ -3,9 +3,9 @@ import ClientCard from "react-tinder-card";
 import "./PossibleClientsList.css";
 import Graph from "../../scripts/graph.js";
 import { mergeSort } from "../../scripts/merge.js";
-import ClientsModal from '../ClientsModal/ClientsModal';
-import Top from '../Top/Top';
-import usersMock from '../../data/users-mock';
+import ClientsModal from "../ClientsModal/ClientsModal";
+import Top from "../Top/Top";
+import usersMock from "../../data/users-mock";
 
 const data = usersMock;
 
@@ -16,12 +16,12 @@ function PossibleClientsList() {
   const [isOpenModal, setIsOpenModal] = useState(null);
 
   const swiped = (direction, newClient) => {
-    if (direction = 'right') {
+    if (direction === "right") {
       const c = clients;
       c.push(newClient);
       setClients(c);
-    } 
-  }
+    }
+  };
 
   async function getAndPrepairData() {
     const peopleList = data;
@@ -45,16 +45,15 @@ function PossibleClientsList() {
         g.addEdge(`${p1}`, p.id, p.distance);
       }
 
-      return { 
-        id: p.id, 
-        distance: p.distance 
+      return {
+        id: p.id,
+        distance: p.distance,
       };
     });
 
     setDistance(d);
     g.dijkstra(d[0].id);
   }
-
 
   async function ordering(pl) {
     let distances = await pl.map((p) => {
@@ -78,44 +77,63 @@ function PossibleClientsList() {
     setPossibleClients(ordpl);
   }
 
-
   useEffect(() => {
     getAndPrepairData();
   }, []);
 
+  console.log();
   return (
     <>
       <Top onPressButton={() => setIsOpenModal(!isOpenModal)} />
       <div>
-        <div>
-          {possibleClients && possibleClients.length &&
+        <div className="divCard">
+          {possibleClients &&
+            possibleClients.length &&
             possibleClients.map((possibleClient) => (
               <>
-              <ClientCard
-                className="swipe"
-                key={possibleClient.id}
-                onSwipe={(dir) => swiped(dir, possibleClient)}
-              >
-                <div
-                  className="card"
-                  style={{ backgroundImage: `url(${possibleClient.avatar})` }}
+                <ClientCard
+                  className="swipe"
+                  key={possibleClient.id}
+                  onSwipe={(dir) => swiped(dir, possibleClient)}
                 >
-                </div>
-                <div style={{ backgroundColor: 'white', width: '100%'}}>
-                  <p>veículo: {possibleClient.vehicle}</p>
-                  <p>proprietario: {possibleClient.name}</p>
-                  <p>distancia localização: {possibleClient.distance}km</p>
-                  <p>idade proprietario: {possibleClient.age}</p>
-                </div>
-                <p><br></br>arraste pro lado esquerdo para adicionar potencial cliente</p>
-                <p>arraste pro lado direito para pular potencial cliente</p>
-              </ClientCard>
+                  <div
+                    className="card"
+                    style={{
+                      backgroundImage: `url(${possibleClient.avatar})`,
+                    }}
+                  >
+                    <div className="content">
+                      <p>Veículo: {possibleClient.vehicle}</p>
+                      <p>Distancia localização: {possibleClient.distance}km</p>
+                      <p>Proprietario: {possibleClient.name}</p>
+                      <p>Idade proprietario: {possibleClient.age}</p>
+                    </div>
+                  </div>
+                </ClientCard>
               </>
             ))}
         </div>
+        <div className="dragTip">
+          <div>
+            <p>
+              {`<--`} Arraste para o lado esquerdo para pular potencial cliente
+            </p>
+          </div>
+          <div>
+            <p>
+              Arraste para o lado direito para adicionar potencial cliente{" "}
+              {`-->`}
+            </p>
+          </div>
+        </div>
       </div>
 
-      { isOpenModal && (<ClientsModal users={clients} />)}
+      {isOpenModal && (
+        <ClientsModal
+          users={clients}
+          handleClose={() => setIsOpenModal(false)}
+        />
+      )}
     </>
   );
 }
